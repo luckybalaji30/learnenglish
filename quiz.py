b9d9f14a193afec2a885211e52e463db642fcf38
NSPC911 <notsoprocoder911@gmail.com>
2024-06-14 09:59:44 +0800

    Uses os.getcwd() instead of a fixed path

--- quiz.py
+++ quiz.py
@@ -10,8 +10,12 @@ import pandas as pd
 
 app = Flask(__name__)
 
+try:
+    os.mkdir(f"{os.getcwd()}/upload")
+except:
+    pass
 # global variables
-UPLOAD_FOLDER = '/Users/balajis/Balaji/Learning/Python/pranav01/'
+UPLOAD_FOLDER = os.getcwd()
 ALLOWED_EXTENSIONS = {'csv'}
 filename="pranavawordlist.csv"
 
